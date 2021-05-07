097ecb12be09bda5d8f60be44f56a82930514d3b
jashjchoi <jashjchoi@gmail.com>
2021-05-07 19:05:28 +0000

    Modified errorhandler and import jsonify

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Starts Flask web app """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
 app = Flask(__name__)
@@ -16,9 +16,9 @@ def teardown(exception):
 
 
 @app.errorhandler(404)
-def not_found_404(err):
+def not_found(error):
     """returns page not found 404 error"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
